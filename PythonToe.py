a617e43244bc2545fe6d83f72f439c0b8e00951c
Ayodele Alakija <alakijaayo@gmail.com>
2022-02-15 20:45:00 +0000

    Adding start funciton

---
+++ PythonToe.py
@@ -0,0 +1,7 @@
+def start():
+  print("Welcome to Tic Tac Toe/Noughts and Crosses!")
+  print("This is a two player game")
+  print("To pick a square, please put the initials. For example, bottom left will be 'BL'")
+  print("Let's Begin!")
+
+start();
