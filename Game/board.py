b3d395ba3732a0185d557fed854c7b4ab15b618b
Ayodele Alakija <alakijaayo@gmail.com>
2022-02-15 22:42:22 +0000

    Adding player and board classes

---
+++ Game/board.py
@@ -0,0 +1,7 @@
+class Board:
+
+  game = False
+
+  def  __init__(self, input, output, ):
+    self.input = input
+    self.output = output
---
+++ Game/player.py
@@ -0,0 +1,10 @@
+from Game import myboard
+
+class Player:
+
+  def __init__(self, name, output, input):
+    self.name = name
+    self.output = output
+    self.input = input
+
+    
